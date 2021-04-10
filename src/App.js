a31223654000993f3b3532207fd46f3002121efe
rdogm13 <drg6525@yahoo.com>
2021-04-09 17:41:59 -0700

    added header / footer and react-bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <p>My App</p>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1> Welcome to ProShop! </h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
