335cb8a2a54ced1cbbcf1fd9661825e5e1706776
rdogm13 <drg6525@yahoo.com>
2021-04-09 20:30:50 -0700

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 import Icons from './components/Icons'
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1> Welcome to ProShop! </h1>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
                 <Container>
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/cart"><FontAwesomeIcon icon="shopping-cart"/ >Cart</Nav.Link>
-                            <Nav.Link href="/login"><FontAwesomeIcon icon="user"/>Login</Nav.Link>
+
+                            <LinkContainer to="/cart">
+                                <Nav.Link><FontAwesomeIcon icon="shopping-cart" />Cart</Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to="/login">
+                                <Nav.Link><FontAwesomeIcon icon="user" />Login</Nav.Link>
+                            </LinkContainer>
+                            
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
