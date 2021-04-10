35fa8fb50fa5e7a0c42e0573e773b134192fd2c8
rdogm13 <drg6525@yahoo.com>
2021-04-09 20:17:57 -0700

    added star rating to products

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -3,12 +3,19 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 import {
     faShoppingCart,
+    faStar,
+    faStarHalfAlt,
     faUser
 } from '@fortawesome/free-solid-svg-icons'
+
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
 
 const Icons = () => {
     return library.add(
         faShoppingCart,
+        faStar,
+        farStar,
+        faStarHalfAlt,
         faUser
     );
 }
---
+++ src/components/Rating.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Rating({value, text, color}) {
+
+    return (
+        <div className="rating">
+            <span>
+                <FontAwesomeIcon style={{ color }}
+                    icon={ [ value >= 0.5 ? 'fas' : 'far',
+                         value >= 1 ? "star" : value >= 0.5 ? "star-half-alt" : "star"
+                         ]}></FontAwesomeIcon>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{ color }}
+                    icon={ [ value >= 1.5 ? 'fas' : 'far',
+                         value >= 2 ? "star" : value >= 1.5 ? "star-half-alt" : "star"
+                         ]}></FontAwesomeIcon>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{ color }}
+                    icon={ [ value >= 2.5 ? 'fas' : 'far',
+                         value >= 3 ? "star" : value >= 2.5 ? "star-half-alt" : "star"
+                         ]}></FontAwesomeIcon>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{ color }}
+                    icon={ [ value >= 3.5 ? 'fas' : 'far',
+                         value >= 4 ? "star" : value >= 3.5 ? "star-half-alt" : "star"
+                         ]}></FontAwesomeIcon>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{ color }}
+                    icon={ [ value >= 4.5 ? 'fas' : 'far',
+                         value >= 5 ? "star" : value >= 4.5 ? "star-half-alt" : "star"
+                         ]}></FontAwesomeIcon>
+            </span>
+        </div>
+    )
+}
+
+export default Rating
