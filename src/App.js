35c306cdcc97a68f532ea50a4fa852cc3efd12ab
rdogm13 <drg6525@yahoo.com>
2021-04-09 19:43:10 -0700

    dummy product data and rendered product cards for HomeScreen view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 import Icons from './components/Icons'
 
 
@@ -12,6 +14,7 @@ function App() {
       <main className="py-3">
         <Container>
           <h1> Welcome to ProShop! </h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
