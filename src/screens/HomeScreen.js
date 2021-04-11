d5169aee9f79812084db8da1a10b70db28a1c6f0
rdogm13 <drg6525@yahoo.com>
2021-04-10 20:04:47 -0700

    loader and alert message for handling product list load

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 
 import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import Product from '../components/Product'
 
 // import products from '../products'
@@ -21,8 +23,8 @@ function HomeScreen() {
         <div>
             <h1>Latest Products</h1>
             {
-                loading ? <h2>Loading...</h2>
-                    : error ? <h3>{error}</h3>
+                loading ? <Loader/>
+                    : error ? <Message variant="danger">{error}</Message>
                         :
                         <Row>
                             {products.map(product => (
