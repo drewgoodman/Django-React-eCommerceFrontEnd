17815649d76862ba439d99792592009ead04ceee
rdogm13 <drg6525@yahoo.com>
2021-04-10 17:52:38 -0700

    dispatch hooked up to homescreen, shows state data

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -6,7 +6,7 @@ import {
     PRODUCT_LIST_FAILURE,
 } from '../constants/productConstants'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
         const { data } = await axios.get('/api/products/')
