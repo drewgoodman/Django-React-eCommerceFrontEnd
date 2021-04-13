b2fd580f76ee7167f755c1fd88f68ef858ef921c
rdogm13 <drg6525@yahoo.com>
2021-04-12 19:58:52 -0700

    created register screen and user registration functionality

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,7 +4,12 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAILURE,
+
     USER_LOGIN_LOGOUT,
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAILURE,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -48,4 +53,49 @@ export const logout = () => (dispatch) => {
     dispatch({
         type: USER_LOGIN_LOGOUT
     })
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post(
+            '/api/users/register/',
+            {
+                'name': name,
+                'email':email,
+                'password': password
+            },
+            config
+            )
+
+        dispatch({
+            type:USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type:USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch(error) {
+        dispatch({
+            type: USER_REGISTER_FAILURE,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+
+        })
+    }
 }
