ed1959e7d349bc87c71bcab295ef7da269267a82
rdogm13 <drg6525@yahoo.com>
2021-04-12 20:41:05 -0700

    added profile screen with API call to populate user details

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
@@ -23,6 +25,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
         </Container>
       </main>
       <Footer />
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -8,6 +8,10 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAILURE,
+
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAILURE,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = { }, action) => {
@@ -40,6 +44,23 @@ export const userRegisterReducer = (state = { }, action) => {
             return { loading: false, userInfo: action.payload }
 
         case USER_REGISTER_FAILURE:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = { user: { } }, action) => {
+
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+
+        case USER_DETAILS_FAILURE:
             return { loading: false, error: action.payload }
 
         default:
