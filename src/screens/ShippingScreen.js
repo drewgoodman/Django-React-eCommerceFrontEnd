2601c75ad6defd0fa27e153b62776ad595158312
rdogm13 <drg6525@yahoo.com>
2021-04-13 17:26:52 -0700

    checkout steps nav element

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Form, Button } from 'react-bootstrap'
 import { saveShippingAddress } from '../actions/cartActions'
 
+import CheckoutSteps from '../components/CheckoutSteps'
 import FormContainer from '../components/FormContainer'
 
 function ShippingScreen({ history }) {
@@ -26,6 +27,7 @@ function ShippingScreen({ history }) {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2/>
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
 
