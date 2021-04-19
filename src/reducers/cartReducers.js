56ef6290e598f8645d50d9b4cc063b0a9a5f7b13
rdogm13 <drg6525@yahoo.com>
2021-04-18 19:08:45 -0700

    place order reducer and actions, clear state

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -2,12 +2,14 @@ import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
-    CART_SAVE_PAYMENT_METHOD
+    CART_SAVE_PAYMENT_METHOD,
+    CART_CLEAR_ITEMS
 } from "../constants/cartConstants"
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
 
     switch (action.type) {
+        
         case CART_ADD_ITEM:
             const item = action.payload
             const existItem = state.cartItems.find(x => x.product === item.product)
@@ -37,6 +39,12 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
             return {
                 ...state,
                 paymentMethod: action.payload
+            }
+        
+        case CART_CLEAR_ITEMS:
+            return {
+                ...state,
+                cartItems: {}
             }
 
         default:
--- src/screens/PlaceOrderScreen.js
+++ src/screens/PlaceOrderScreen.js
@@ -6,8 +6,15 @@ import { Link } from 'react-router-dom'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
 
-function PlaceOrderScreen() {
+function PlaceOrderScreen({history}) {
+
+    const orderCreate = useSelector(state => state.orderCreate)
+    const {order, error, success} = orderCreate
+
+    const dispatch = useDispatch()
     const cart = useSelector(state => state.cart)
 
     cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
@@ -15,8 +22,28 @@ function PlaceOrderScreen() {
     cart.taxPrice = Number((0.082) * cart.itemsPrice).toFixed(2)
     cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2)
 
+    if (!cart.paymentMethod) {
+        history.push('/payment')
+    }
+
+    useEffect(()=> {
+        if (success) {
+            history.push(`/order/${order._id}`)
+            dispatch({ type: ORDER_CREATE_RESET })
+        }
+    }, [success, history])
+
     const placeOrder = () => {
-        console.log("Place order!")
+        dispatch(createOrder({
+            orderItems: cart.cartItems,
+            shippingAddress: cart.shippingAddress,
+            paymentMethod: cart.paymentMethod,
+
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+        }))
     }
     return (
         <div>
@@ -105,6 +132,9 @@ function PlaceOrderScreen() {
                                     </Col>
                                     <Col>${cart.totalPrice}</Col>
                                 </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                { error && <Message variant='danger'>{error}</Message> }
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <Button
